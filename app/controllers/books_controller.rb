50d57b850f34ebd9359ce208d29b27a1eced54e0
ryujihagiwara <dragon7103next@icloud.com>
2020-12-21 22:38:36 +0900

    updateアクション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to books_path
+    else
+       @books = Book.all
+       render :index
+    end
   end
 
   def index
@@ -20,6 +24,9 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
