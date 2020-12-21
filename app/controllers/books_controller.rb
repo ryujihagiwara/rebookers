0180e927b9f3acf5a7542afbc1a76112639cc330
ryujihagiwara <dragon7103next@icloud.com>
2020-12-21 23:00:06 +0900

    create,update時エラーメッセージ設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-       redirect_to books_path
+       redirect_to book_path(@book)
     else
        @books = Book.all
        render :index
@@ -25,8 +25,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+       redirect_to book_path(@book)
+    else
+       render :edit
+    end
   end
 
   def destroy
